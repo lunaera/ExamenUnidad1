ca30261a45401bab1f9ba4a5ce060c2fdbace463
XimenaItzel <itzelx463@gmail.com>
2025-09-25 11:03:00 -0600

    XH

--- js/X.H.js
+++
@@ -1,7 +0,0 @@
-let estados = ["activo", "pendiente", "completado"];
-
-// Reemplazar y guardar los nuevos elementos
-estados.splice(1, 2, "cerrado", "archivado");
-let nuevos = estados.slice(1);
-
-console.log(nuevos); // ["cerrado", "archivado"]
