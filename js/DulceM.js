824b6edb5519f2b82e823bc770140146964ea420
DulceM-RG <1tamaldeverde@gmail.com>
2025-09-25 10:34:41 -0600

    Resolución de problematica

---
+++ js/DulceM.js
@@ -0,0 +1,6 @@
+const nombres = ["David", "Ana", "Dulce", "Alberto", "María"];
+
+const primerNombreConA = nombres.find(nombre => nombre.startsWith("A"));
+
+console.log(primerNombreConA); // "Ana"
+
