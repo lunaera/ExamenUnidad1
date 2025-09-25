9df4922bdd565de44e4f5ae8c5881ec3683a5af1
CesarAle31 <l22920168@voaxaca.tecnm.mx>
2025-09-25 10:27:07 -0600

    agregando script 26

---
+++ js/CesarAle.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, -3, 4, 5];
+const hayNegativo = numeros.some(numero => numero < 0);
+
+console.log(hayNegativo); // Devuelve true porque hay un -3 en el arreglo
+
+
+
+
+//soy Cesar Alejandro Martinez Chavez
