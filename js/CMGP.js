75ccc32175a66eeba0286b1de6654bd44e9a8254
Crys <gcrystalmonserrat@gmail.com>
2025-09-25 10:44:12 -0600

    Solución de mi ejercicio join()

--- js/CMGP.js
+++ js/CMGP.js
@@ -1,3 +1,6 @@
+// Autor: Crystal Monserrat García Pacheco
+// Ejercicio: Convertir arreglo en cadena con join()
+
 const palabras = ["uno", "dos", "tres"];
 const resultado = palabras.join("-");
 console.log(resultado);
