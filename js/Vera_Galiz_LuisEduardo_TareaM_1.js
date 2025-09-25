e1aff7470901eabc666d43f01481cf793985d683
Luna <erahinpachecoluna@gmail.com>
2025-09-25 10:28:34 -0600

    calificando a eduardo

---
+++ js/Vera_Galiz_LuisEduardo_TareaM_1.js
@@ -0,0 +1,13 @@
+const calificaciones = [8, 9, 10];
+
+// Usa reduce() para sumar todos los elementos del arreglo.
+// El 0 es el valor inicial del acumulador.
+const suma = calificaciones.reduce((acumulador, calificacionActual) => acumulador + calificacionActual, 0);
+
+// Divide la suma total por el número de elementos para obtener el promedio.
+const promedio = suma / calificaciones.length;
+
+console.log(`El promedio de las calificaciones es: ${promedio}`);
+
+
+// Excelente chico rebelde!!
