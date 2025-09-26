df9c8a90a724e69955ef903efc4c43aac204eabb
Luna <erahinpachecoluna@gmail.com>
2025-09-25 21:22:36 -0600

    calificado el resto de examenes

--- js/Sabdiel.js
+++ js/Sabdiel.js
@@ -5,4 +5,18 @@ let colores = ["rojo", "verde", "azul", "amarillo"];
 let primerosTres = colores.slice(0, 3);
 
 
-console.log(primerosTres); 
+console.log(primerosTres);
+
+
+// calificado sabdiel, futuro ingeniero
+
+/*
+    instrucciones:
+    1. si ya actualizaste tu main, significa que estas biendo estas instrucciones
+    2. ahora actualiza tu main2
+    3. sube los cambios a tu rama
+    4. listo, debes enviarme 3 fotos a mi whasapp personal: 
+        a) evidencia del main sobre lo que te puse en la calificación (osea esto 👈)
+        b) evidencia del main2 (el otro archivo que actualizaste)
+        c) evidencia de tu repositorio donde igualmente se vea lo que te califiqué
+*/
--- js/aamc.js
+++ js/aamc.js
@@ -10,3 +10,16 @@ console.log(tieneArroba);
 
 
 // Listo Alejandro Morales, calificado
+
+
+
+/*
+    instrucciones:
+    1. si ya actualizaste tu main, significa que estas biendo estas instrucciones
+    2. ahora actualiza tu main2
+    3. sube los cambios a tu rama
+    4. listo, debes enviarme 3 fotos a mi whasapp personal: 
+        a) evidencia del main sobre lo que te puse en la calificación (osea esto 👈)
+        b) evidencia del main2 (el otro archivo que actualizaste)
+        c) evidencia de tu repositorio donde igualmente se vea lo que te califiqué
+*/
